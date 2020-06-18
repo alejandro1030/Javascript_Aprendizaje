e53a31a463f2de2e796eee99236464db3f06f32a
Jorge Norbey Alejandro Gonzalez Montealegre <jorgenalejandrogm@gmail.com>
2020-06-17 21:35:10 -0500

    Cambios aplicados en ECMAScript 8

---
+++ src/es8/index.js
@@ -0,0 +1,32 @@
+// ECMASript 8
+// Este cambio permite devolver la clave y los valores de una matriz
+
+const data ={
+    frontend: 'Oscar',
+    backend: 'Alejandro',
+    design: 'Vivian'
+}
+
+// como convertir este objeto a una matriz
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length);
+
+// object values, devuelve los valores de un objeto a un arreglo
+const data = {
+    frontend: 'Oscar',
+    backend: 'Alejandro',
+    design: 'Vivian'
+}
+
+// como hacer la transformación
+const values = Object.values(data);
+console.log(values);
+console.log(values.length);
+
+// padding como anteponer a agregar una cadena de vacía o elementos a un string
+const string = 'hello';
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(12, ' -----'));
+console.log(string.padEnd(12, '  -----'))
+// esto es util para frontend
